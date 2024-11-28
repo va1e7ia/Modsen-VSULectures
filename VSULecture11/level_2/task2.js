78cca2912445269f3d8fc864b17b05743024636a
Valeria Mikhalevich <valeerriam@gmail.com>
2024-11-28 13:05:24 +0300

    fix

--- VSULecture11/level_2/task2.js
+++ VSULecture11/level_2/task2.js
@@ -1,23 +1,21 @@
 function Pet(name, age) {
   this.name = name;
   this.age = age;
-  this.describe = function () {
-    return `Name: ${this.name}, age: ${this.age}`;
-  };
-  this.isOld = function () {
-    if (this.age > 10) {
-      return true;
-    } else {
-      return false;
-    }
-  };
 }
+
+Pet.prototype.describe = function () {
+  return `Name: ${this.name}, age: ${this.age}`;
+};
+
+Pet.prototype.isOld = function () {
+  return this.age > 10;
+};
 
 Pet.compareAges = function (pet1, pet2) {
   if (pet1.age > pet2.age) {
     return `${pet1.name} is elder than ${pet2.name}!`;
   } else {
-    return `${pet2.name} is elder ${pet1.name}!`;
+    return `${pet2.name} is elder than ${pet1.name}!`;
   }
 };
 
@@ -25,6 +23,8 @@ function Dog(name, age, breed) {
   Pet.call(this, name, age);
   this.breed = breed;
 }
+
+Dog.prototype = Object.create(Pet.prototype);
 
 Dog.prototype.describe = function () {
   return `Name: ${this.name}, age: ${this.age}, breed: ${this.breed}`;
@@ -38,18 +38,23 @@ Dog.prototype.fetch = function (item) {
   return `${this.name} fetches ${item}.`;
 };
 
-// function GuardDog(name, age, breed, trainingLevel) {
-//   Dog.call(this, name, age, breed);
-//   this.trainingLevel = trainingLevel;
-//   this.guard = function () {
-//     return `${this.name} guards territory at the ${this.trainingLevel} level`;
-//   };
-//   if (trainingLevel > 5) {
-//     this.bark = function () {
-//       return `${this.name} is barking loud!`;
-//     };
-//   }
-// }
+function GuardDog(name, age, breed, trainingLevel) {
+  Dog.call(this, name, age, breed);
+  this.trainingLevel = trainingLevel;
+}
+
+GuardDog.prototype = Object.create(Dog.prototype);
+
+GuardDog.prototype.guard = function () {
+  return `${this.name} guards territory at the ${this.trainingLevel} level`;
+};
+
+GuardDog.prototype.bark = function () {
+  if (this.trainingLevel > 5) {
+    return `${this.name} is barking loud!`;
+  }
+  return Dog.prototype.bark.call(this);
+};
 
 const cat = new Pet("Luna", 2);
 console.log(cat.name);
@@ -70,17 +75,17 @@ console.log(dog.fetch("toy"));
 
 console.log("*****************************************");
 
-// const guardDog = new GuardDog("Muhtar", 11, "Terrier", 6);
-// console.log(guardDog.name);
-// console.log(guardDog.age);
-// console.log(guardDog.breed);
-// console.log(guardDog.trainingLevel);
-// console.log(guardDog.describe());
-// console.log("Old?: " + guardDog.isOld());
-// console.log(guardDog.bark());
-// console.log(guardDog.guard());
-// console.log(guardDog.fetch("bone"));
+const guardDog = new GuardDog("Muhtar", 11, "Terrier", 6);
+console.log(guardDog.name);
+console.log(guardDog.age);
+console.log(guardDog.breed);
+console.log(guardDog.trainingLevel);
+console.log(guardDog.describe());
+console.log("Old?: " + guardDog.isOld());
+console.log(guardDog.bark());
+console.log(guardDog.guard());
+console.log(guardDog.fetch("bone"));
 
 console.log("*****************************************");
 
-// console.log(Pet.compareAges(dog, guardDog));
+console.log(Pet.compareAges(dog, guardDog));
