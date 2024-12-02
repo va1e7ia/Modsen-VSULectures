3689b906e1f299d988402e48163c55059d1a6aca
Valeria Mikhalevich <valeerriam@gmail.com>
2024-12-02 14:19:15 +0300

    add task2 lvl2, remade task2 lvl1

--- VSULecture12/level 1/task2.js
+++ VSULecture12/level 1/task2.js
@@ -6,15 +6,25 @@ class Person {
     this.#age = age;
   }
 
-  getName() {
+  // getName() {
+  //   return this.#name;
+  // }
+
+  // getAge() {
+  //   return this.#age;
+  // }
+
+  get name() {
     return this.#name;
   }
 
-  getAge() {
+  get age() {
     return this.#age;
   }
 }
 
 const person = new Person("Me", 19);
-console.log(person.getName());
-console.log(person.getAge());
+// console.log(person.getName());
+// console.log(person.getAge());
+console.log(person.name);
+console.log(person.age);
---
+++ VSULecture12/level 2/task2.js
@@ -0,0 +1,52 @@
+class Shape {
+  static count = 0;
+  #name;
+
+  constructor(name) {
+    this.#name = name;
+    Shape.count++;
+  }
+
+  getArea() {
+    return `Тут должен быть переопределенный метод)`;
+  }
+
+  static getCount() {
+    return Shape.count;
+  }
+
+  getName() {
+    return this.#name;
+  }
+}
+
+class Rectangle extends Shape {
+  constructor(name, width, height) {
+    super(name);
+    this.width = width;
+    this.height = height;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+}
+
+class Circle extends Shape {
+  constructor(name, radius) {
+    super(name);
+    this.radius = radius;
+  }
+
+  getArea() {
+    return Math.PI * this.radius * this.radius;
+  }
+}
+
+const rectangle = new Rectangle("Прямоугольник", 5, 10);
+console.log(`${rectangle.getName()} имеет площадь: ${rectangle.getArea()}`);
+
+const circle = new Circle("Круг", 3);
+console.log(`${circle.getName()} имеет площадь: ${circle.getArea()}`);
+
+console.log(`Количество созданных фигур: ${Shape.getCount()}`);
